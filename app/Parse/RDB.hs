69d3439b601d4527f1db5c18f7a0d49aba017a3d
Adrian Hornung <adrian.hornung@yahoo.de>
2023-10-31 14:57:56 +0100

    Partial update

--- app/Parse/RDB.hs
+++ app/Parse/RDB.hs
@@ -10,7 +10,8 @@ module Parse.RDB (
 ) where
 
 import Control.Monad (unless)
-import Data.Int (Int32)
+import Data.Bits (Bits (..), shiftL, (.&.))
+import Data.Int (Int32, Int64)
 import Data.Word (Word64, Word8)
 import Prelude hiding (readFile)
 
@@ -19,6 +20,7 @@ import Data.Binary.Get qualified as G
 import Data.ByteString.Lazy qualified as BL
 import Data.ByteString.Lazy.Char8 qualified as BLC
 import Data.Map.Strict qualified as M
+import Debug.Trace (trace)
 import System.Directory qualified as Dir
 
 type MyGet a = E.ExceptT BL.ByteString G.Get a
@@ -69,7 +71,7 @@ type MyGet a = E.ExceptT BL.ByteString G.Get a
 -}
 
 data RDB = RDB
-    { rdbVersionNr :: BL.ByteString
+    { rdbVersionNr :: Int
     , auxField :: Maybe AuxField
     , dbs :: [Db]
     , fileCheckSum :: BL.ByteString
@@ -84,7 +86,7 @@ data AuxField = AuxField
     }
     deriving (Show)
 
-data Db = DbSelector
+data Db = Db
     { databaseNr :: Int32
     , store :: KVStore
     , expiryStore :: KVExpiryStore
@@ -127,15 +129,125 @@ toValueType v = case v of
     14 -> Just ListZipListEncoding
     _ -> Nothing
 
+data OpCodes
+    = EOF
+    | SELECTDB
+    | EXPIRETIME
+    | EXPIRETIMEMS
+    | RESIZEDB
+    | AUX
+    | NONE
+    deriving (Show, Eq, Enum)
+
+parseOpcode :: (Eq a, Num a) => a -> OpCodes
+parseOpcode opCode = case opCode of
+    0xFF -> EOF
+    0xFE -> SELECTDB
+    0xFD -> EXPIRETIME
+    0xFC -> EXPIRETIMEMS
+    0xFB -> RESIZEDB
+    0xFA -> AUX
+    _ -> NONE
+
+getOpcode :: MyGet OpCodes
+getOpcode = parseOpcode <$> getWord8
+
+getWord8 :: MyGet Word8
+getWord8 = E.lift G.getWord8
+
+getByteString :: Int64 -> MyGet BL.ByteString
+getByteString = E.lift . G.getLazyByteString
+
+{-
+    Bits 	How to parse
+    00 	    The next 6 bits represent the length
+    01 	    Read one additional byte. The combined 14 bits represent the length
+    10 	    Discard the remaining 6 bits. The next 4 bytes from the stream represent the length
+    11 	    The next object is encoded in a special format. The remaining 6 bits indicate the format.
+            May be used to store numbers or Strings, see String Encoding
+-}
+
+getVariableLenNr :: MyGet Int64
+getVariableLenNr = do
+    lRaw <- getWord8
+    let m = lRaw `shiftL` 6
+    let mRes = lRaw .&. complement (0b11 `shiftR` 6)
+    case m of
+        0b00 -> pure $ fromIntegral mRes
+        0b01 -> do
+            n <- getWord8
+            let nInt64 = fromIntegral n :: Int64
+                mResInt64 = fromIntegral mRes :: Int64
+            pure $ (mResInt64 `shiftL` 8) .|. nInt64
+        0b10 -> fromIntegral <$> E.lift G.getWord32be
+        0b11 -> todo
+        _ -> E.throwError $ "Impossible lenght detected... No clue how this case will ever be called. <" <> BLC.pack (show m) <> ">"
+
+getDB :: MyGet Db
+getDB = do
+    dbId <- getVariableLenNr
+    resizedbFildIndicatorOp <- getOpcode
+    unless (RESIZEDB == resizedbFildIndicatorOp) $ E.throwError "Missing Resize DB Indicator Field"
+    lenHashTable <- getVariableLenNr
+    lenExpireHashTable <- getVariableLenNr
+    pure
+        Db
+            { databaseNr = fromIntegral dbId
+            , store = mempty
+            , expiryStore = mempty
+            }
+
+getDBs :: MyGet [Db]
+getDBs = inner []
+  where
+    dbHelper acc dbSelOp = case dbSelOp of
+        SELECTDB -> (inner . (: acc)) =<< getDB
+        EOF -> pure $ reverse acc
+        _ -> E.throwError $ "unexpected database database selector <" <> BLC.pack (show (fromEnum dbSelOp)) <> ">"
+    inner acc = dbHelper acc =<< getOpcode
+
+getAuxField :: MyGet (Maybe AuxField)
+getAuxField = do
+    auxField <- getOpcode
+    unless (AUX == auxField) $ E.throwError "Missing aux field"
+    E.lift skipIt
+  where
+    skipIt = do
+        v <- G.lookAhead G.getWord8
+        if SELECTDB /= parseOpcode v
+            then do
+                _ <- G.getWord8
+                skipIt
+            else pure Nothing
+
 checkRedisHeader :: MyGet ()
 checkRedisHeader = do
-    r <- E.lift $ G.getLazyByteString 5
+    r <- getByteString 5
     unless ("REDIS" == r) $ E.throwError "Missing magic string"
 
-process :: MyGet a
-process = do 
+getRedisVersionNr :: MyGet Int
+getRedisVersionNr = do
+    verDecStr <- getByteString 4
+    let ver = BLC.readInt verDecStr
+    maybe (E.throwError "Unable to parse Redis Version Nr.") (pure . fst) ver
+
+getChecksum :: MyGet BL.ByteString
+getChecksum = getByteString 8
+
+process :: MyGet RDB
+process = do
     checkRedisHeader
-    todo
+    version <- getRedisVersionNr
+    auxField <- getAuxField
+    dbs <- getDBs
+    checkSum <- getChecksum
+    pure
+        RDB
+            { rdbVersionNr = version
+            , auxField = auxField
+            , dbs = dbs
+            , fileCheckSum = checkSum
+            }
 
 readFile :: FilePath -> IO (Either BL.ByteString RDB)
 readFile filePath = do
